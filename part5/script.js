5d57ec94c60cceff74e03a3a841b1a2461ba4e24
Ashutosh <78640870+ashut814@users.noreply.github.com>
2022-05-14 13:41:01 +0530

    Add files via upload

---
+++ part5/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+
+const openModal = function(){
+  console.log('btn clicked');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+
