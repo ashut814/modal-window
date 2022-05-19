f1f35f6fd683fe7c84231533213ba28032457747
ashutosh <adityatripathi1011@gmail.com>
2022-05-19 23:02:45 +0530

    f

--- script.js
+++ script.js
@@ -5,22 +5,31 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-
-const openModal = function(){
-  console.log('btn clicked');
+const openModal = function () {
+  console.log("btn clicked");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
-}
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener("click", openModal);
 
 const closeModal = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
+// handling keypress events
 
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  if (event.key === "Escape") {
+    if(!modal.classList.contains('hidden')){
+      closeModal();
+      
+    }
+  }
+});
